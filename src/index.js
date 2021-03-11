990e666707e5e64965091ac36ee4c1dac2f2da8f
Chase Smith <chasesmith@berkeley.edu>
2021-03-10 17:34:39 -0800

    Updates to rpc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const {ProximaDBServer} = require('./server')
+const {ProximaDBServer} = require('./newServer.js')
 
 function main() {
   ip = "0.0.0.0"
   port = process.env.PORT || "50051"
-  db_path = ""
+  db_path = "./db"
   hash = ""
   bits = ""
   proximaServer = new ProximaDBServer({"ip": ip, "port" : port, "db_path": db_path, "hash": hash, "bits": bits})
