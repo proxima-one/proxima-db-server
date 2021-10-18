ac7093438aa78c741ee142854421a8c6071499ea
Chase Smith <chasesmith@berkeley.edu>
2021-10-18 08:41:56 -0700

    Github tests updates for roots

--- jest.config.js
+++ jest.config.js
@@ -116,7 +116,9 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "./src"
+    "./src",
+    "./test",
+    "./src/http"
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
