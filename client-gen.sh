470e9e21c0526f8fcf078e2053ba62b74cf34dbc
Chase Smith <chasesmith@berkeley.edu>
2021-12-23 11:47:38 +0000

    Client Update

--- client-gen.sh
+++ client-gen.sh
@@ -6,5 +6,10 @@ yarn swagger-autogen
 ## Re-generate the Clients 
 #typescript-node
 openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g typescript-node -o ./src/clients/typescript
+chmod +x ./src/clients/typescript/git_push.sh
+./src/clients/typescript/git_push.sh proxima-one proximadb-client-js "Client Update" "github.com"
+
 #go
 openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g go -o ./src/clients/go
+chmod +x ./src/clients/go/git_push.sh
+./src/clients/go/git_push.sh proxima-one proximadb-client-go "Client Update" "github.com"
