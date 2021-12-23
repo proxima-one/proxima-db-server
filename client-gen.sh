c83045b9768e1610362ce671ba6408fbdc5a6213
Chase Smith <chasesmith@berkeley.edu>
2021-12-23 12:23:21 +0000

    Autogeneration and deployment for openapi client updates

--- client-gen.sh
+++ client-gen.sh
@@ -3,13 +3,32 @@
 # Re-generate the Swagger Documentation
 yarn swagger-autogen
 
-## Re-generate the Clients 
-#typescript-node
-openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g typescript-node -o ./src/clients/typescript
-chmod +x ./src/clients/typescript/git_push.sh
-./src/clients/typescript/git_push.sh proxima-one proximadb-client-js "Client Update" "github.com"
+# remove Client Libs 
+rm -rf ./src/clients
 
+## Re-generate the Clients for REST
+
+#javascript 
+openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g javascript -o ./src/clients/javascript
+chmod +x ./src/clients/javascript/git_push.sh
+cd src/clients/javascript
+./git_push.sh proxima-one proximadb-client-js "Client Update" "github.com" && git push -f origin master && yarn publish
+cd ../../..
+#typescript
+openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g typescript -o ./src/clients/typescript
+chmod +x ./src/clients/typescript/git_push.sh
+cd src/clients/typescript
+./git_push.sh proxima-one proximadb-client-ts "Client Update" "github.com" && git push -f origin master && yarn publish
+cd ../../..
 #go
 openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g go -o ./src/clients/go
 chmod +x ./src/clients/go/git_push.sh
-./src/clients/go/git_push.sh proxima-one proximadb-client-go "Client Update" "github.com"
+cd src/clients/go
+./git_push.sh proxima-one proximadb-client-go "Client Update" "github.com" && git push -f origin master
+cd ../../..
+#rust
+openapi-generator generate -i ./src/endpoints/swagger/swagger_output.json -g rust -o ./src/clients/rust
+chmod +x ./src/clients/rust/git_push.sh
+cd src/clients/rust
+./git_push.sh proxima-one proximadb-client-rust "Client Update" "github.com" && git push -f origin master
+cd ../../..
