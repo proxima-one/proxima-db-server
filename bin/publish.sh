711d689101f7d30e27c2823cc4f36db538d76f28
Chase Smith <chasesmith@berkeley.edu>
2022-01-21 06:59:50 -0800

    CI/CD testing

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,4 +9,4 @@ docker push us-west4-docker.pkg.dev/wired-framework-311403/proximaone/proxima-db
 docker tag proximaone/proxima-db:$version chasesmith95/proxima-db-server:$version
 docker push chasesmith95/proxima-db-server:$version
 
-./bin/git_push.sh proxima-one proximadb-client-grpc-js "Client Update" "github.com" "../src/gen" && git push -f origin master
+./git_push.sh proxima-one proximadb-client-grpc-js "Client Update" "github.com" "../src/gen" && git push -f origin master
