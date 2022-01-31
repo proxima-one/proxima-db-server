850169eecf39849908540c726a945b71bec59285
Chase Smith <chasesmith@berkeley.edu>
2022-01-31 09:54:21 -0800

    Adding package json

--- src/api/grpc/newServer.js
+++ src/api/grpc/newServer.js
@@ -1,7 +1,7 @@
 const grpc = require("grpc");
 const { Database, Table } = require("proxima-db");
 const protoLoader = require("@grpc/proto-loader");
-const packageDefinition = protoLoader.loadSync("./src/proto/proxima.proto");
+const packageDefinition = protoLoader.loadSync("./src/proto/v1/proxima.proto");
 const proximaProto = grpc.loadPackageDefinition(packageDefinition);
 const {
   parseProof,
