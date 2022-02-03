aaa678156981a557d064f7a689c883ec70df3914
chasesmith95 <chasesmith95@users.noreply.github.com>
2022-02-03 14:42:33 +0000

    Ci/cd (#15)
    * CI/CD testing
    
    * Updates for pull request to ci workflow
    
    * Adding package json
    
    * Adding package json
    
    * Adding package json
    
    * Database test:
    
    * Update workflows
    
    * Update workflows
    
    * Update workflows
    
    * Update workflows

--- jest.config.js
+++ jest.config.js
@@ -157,7 +157,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/clients/"
+    // "/clients/"
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
